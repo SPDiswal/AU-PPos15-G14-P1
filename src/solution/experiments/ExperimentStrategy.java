741f2bdcf3ed354c45d314f32df8b1e1058b58cb
Steffen Diswal <steffen.diswal@gmail.com>
2015-09-16 15:56:38 +0200

    Experiments can throw exceptions.

--- src/solution/experiments/ExperimentStrategy.java
+++ src/solution/experiments/ExperimentStrategy.java
@@ -1,10 +1,11 @@
 package solution.experiments;
 
+import java.io.IOException;
 import java.util.*;
 
 public interface ExperimentStrategy
 {
-    List<DoublePair> runExperiment();
+    List<DoublePair> runExperiment() throws IOException;
     
     List<DoublePair> aggregateResults(Set<List<DoublePair>> results);
 }
