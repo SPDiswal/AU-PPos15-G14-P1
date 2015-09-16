ab4465a3af251f5f8093289635d0431e4cd06327
Steffen Diswal <steffen.diswal@gmail.com>
2015-09-16 15:55:12 +0200

    Added file with common constants.

---
+++ src/solution/Constants.java
@@ -0,0 +1,16 @@
+package solution;
+
+public class Constants
+{
+    public static final int OFFLINE_SIZE = 25;
+    public static final int ONLINE_SIZE = 5;
+    
+    public static final String ONLINE_TRACES = "data/MU.1.5meters.online.trace";
+    public static final String OFFLINE_TRACES = "data/MU.1.5meters.offline.trace";
+    public static final String ACCESS_POINT_POSITIONS = "data/MU.AP.positions";
+    
+    private static final String OUTPUT_PATH = "output/";
+    private static final String EXPERIMENT_OUTPUT_PATH = "output/experiments/";
+    
+    
+}
